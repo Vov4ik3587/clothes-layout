10b74d5a0c0cd66f0474d99e0c977fc05c568615
Vov4ik3587 <galstyan.vova@gmail.com>
2022-07-18 13:59:42 +0700

    Улучшение кода зума

--- zoom.js
+++ zoom.js
@@ -1,20 +1,30 @@
 $(document).ready(function() {
     
-    $('img.product__mini-pic1').mouseenter(function () {
-        $('img.product__big-pic').attr('src', './img/pic1.jpg')
-    }).mouseleave(function () {
-        $('img.product__big-pic').attr('src', './img/default.jpg')
-    })
+    const defaultSrc = './img/default.jpg';
+    const mainPic = $('img.product__big-pic');
 
-    $('img.product__mini-pic2').mouseenter(function () {
-        $('img.product__big-pic').attr('src', './img/pic2.jpg')
+    $('.product__mini-pic').mouseenter(function() {
+        const currentSrc = $(this).attr('src');
+        mainPic.attr('src', currentSrc);
     }).mouseleave(function () {
-        $('img.product__big-pic').attr('src', './img/default.jpg')
-    })
+        mainPic.attr('src', defaultSrc);
+    });
 
-    $('img.product__mini-pic3').mouseenter(function () {
-        $('img.product__big-pic').attr('src', './img/pic3.jpg')
-    }).mouseleave(function () {
-        $('img.product__big-pic').attr('src', './img/default.jpg')
-    })
+    // $('img.product__mini-pic1').mouseenter(function () {
+    //     mainPic.attr('src', './img/pic1.jpg')
+    // }).mouseleave(function () {
+    //     mainPic.attr('src', defaultSrc)
+    // })
+
+    // $('img.product__mini-pic2').mouseenter(function () {
+    //     mainPic.attr('src', './img/pic2.jpg')
+    // }).mouseleave(function () {
+    //     mainPic.attr('src', defaultSrc)
+    // })
+
+    // $('img.product__mini-pic3').mouseenter(function () {
+    //     mainPic.attr('src', './img/pic3.jpg')
+    // }).mouseleave(function () {
+    //     mainPic.attr('src', defaultSrc)
+    // })
 });
